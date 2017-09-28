ca656cb66562524b9ce57a41a30f5a48501ab42d
Arshin Jain <arshinx@users.noreply.github.com>
2017-09-28 11:45:49 -0400

    Indentation

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,37 +2,37 @@
 
 // Bio
 var bio = {
-    "name": "Arshin Jain",
-    "role": "Web Designer",
-    "contacts": {
-        "mobile": "+650.121.9898",
-        "email": "arshin.jain@outlook.com",
-        "github": "arshinx",
-        "twitter": "@arshin",
-        "location": "Palo Alto, CA"
-    },
-    "skills": ["Presentations", "Web Design", "Tennis"],
-    "welcomeMessage": "Greetings!",
-    "biopic": "images/fry.jpg"
+  "name": "Arshin Jain",
+  "role": "Web Designer",
+  "contacts": {
+      "mobile": "+650.121.9898",
+      "email": "arshin.jain@outlook.com",
+      "github": "arshinx",
+      "twitter": "@arshin",
+      "location": "Palo Alto, CA"
+  },
+  "skills": ["Presentations", "Web Design", "Tennis"],
+  "welcomeMessage": "Greetings!",
+  "biopic": "images/fry.jpg"
 };
 
 // Education
 var education = {
-    "schools": [{
-        "name": "CMU",
-        "location": "Pittsburgh",
-        "degree": "CS",
-        "dates": "Aug 2017 - Present",
-        "majors": ["Web Development"],
-        "url": "udacity.com"
-    }],
+  "schools": [{
+      "name": "CMU",
+      "location": "Pittsburgh",
+      "degree": "CS",
+      "dates": "Aug 2017 - Present",
+      "majors": ["Web Development"],
+      "url": "udacity.com"
+  }],
 
-    "online": [{
-      "title": "Front-End Web Developer"
-      "dates": "July 2017 - Present",
-      "school": "Udacity",
-      "url": "https://www.udacity.com"
-    }]
+  "online": [{
+    "title": "Front-End Web Developer"
+    "dates": "July 2017 - Present",
+    "school": "Udacity",
+    "url": "https://www.udacity.com"
+  }]
 }
 
 // Work
